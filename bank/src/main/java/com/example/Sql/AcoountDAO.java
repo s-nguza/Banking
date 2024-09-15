8354bf1a844ba603b1d94f68d73878d65760a346
Siyabonga Nguza <siyabonganguza18@gmail.com>
2024-09-15 18:09:15 +0200

    Adding AccountDAO

---
+++ bank/src/main/java/com/example/Sql/AcoountDAO.java
@@ -0,0 +1,34 @@
+package com.example.Sql;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class AcoountDAO {
+    private static final String DB_URL =  "jdbc:sqlite:banking_application.db";
+
+    public void AccountDetails(int customer_id,String account_type ,double balance){
+        String Sql = "INSERT INTO Accounts(customer_id,account_type,balance) VALUES(?,?,?)";
+
+        try (Connection con = DriverManager.getConnection(DB_URL);
+            PreparedStatement ps = con.prepareStatement(Sql)) {
+                ps.setInt(1, customer_id);
+                ps.setString(2, Sql);
+                ps.setDouble(3, balance);
+
+                ps.executeUpdate();
+                System.out.println("Accoumt added successfully");
+
+
+            }catch (SQLException e){
+                e.printStackTrace();
+
+            }
+
+
+
+    }
+
+
+}
