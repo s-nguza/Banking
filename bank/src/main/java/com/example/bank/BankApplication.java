0769cea95c013b92631dc18d230d2b549533694b
Siyabonga Nguza <siyabonganguza18@gmail.com>
2024-09-15 17:07:28 +0200

    create tables

--- bank/src/main/java/com/example/bank/BankApplication.java
+++ bank/src/main/java/com/example/bank/BankApplication.java
@@ -3,11 +3,15 @@ package com.example.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.Sql.DatabaseInitializer;
+
 @SpringBootApplication
 public class BankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApplication.class, args);
+		 // Initialize SQLite tables
+        DatabaseInitializer.createTables();
 	}
 
 }
