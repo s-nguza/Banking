1d9172c0641b421fa27e765ffa8ae963115fac16
Siyabonga Nguza <siyabonganguza18@gmail.com>
2024-09-17 14:42:22 +0200

    added setter and getters

--- bank/src/main/java/com/example/Sql/Model/Customer.java
+++ bank/src/main/java/com/example/Sql/Model/Customer.java
@@ -26,11 +26,38 @@ public class Customer {
     private String email;
 
     @NotNull
-    private String phone;
+    private String password;
 
     // Relationship with Accounts
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     private List<Account> accounts;
 
     // Constructors, getters, and setters
+    public String getName(){
+        return name;
+    }
+    public void  setName(String name){
+        this.name = name;
+    }
+    
+    public String getSurname(){
+        return surname;
+    }
+    public void setSurname(String surname){
+        this.surname  = surname;
+    }
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getpassword(){
+        return password;
+    }
+    public void setPasssword(String password){
+        this.password= password;
+    }
+
 }
