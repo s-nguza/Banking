a201b0abfb00626af191a68bb584c29efd5a9173
Siyabonga Nguza <siyabonganguza18@gmail.com>
2024-09-22 21:02:52 +0200

    Working on the signin feature

--- bank/src/main/java/com/example/Sql/Model/Account.java
+++ bank/src/main/java/com/example/Sql/Model/Account.java
@@ -1,6 +1,5 @@
 package com.example.Sql.Model;
 
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
@@ -28,8 +27,7 @@ public class Account {
     @OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
     private List<Transaction> transactions;
 
-    // Constructors, getters, and setters
-
+    // Constructors
     public Account() {
     }
 
@@ -40,4 +38,44 @@ public class Account {
     }
 
     // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
 }
--- bank/src/main/java/com/example/Sql/Model/Transaction.java
+++ bank/src/main/java/com/example/Sql/Model/Transaction.java
@@ -1,6 +1,5 @@
 package com.example.Sql.Model;
 
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -27,8 +26,7 @@ public class Transaction {
     @JoinColumn(name = "account_id", nullable = false)
     private Account account;
 
-    // Constructors, getters, and setters
-
+    // Constructors
     public Transaction() {
     }
 
@@ -40,5 +38,43 @@ public class Transaction {
     }
 
     // Getters and Setters
-}
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDateTime transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+}
