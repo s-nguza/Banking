bded4b5817909d87cfbfb2f87d929e25766ca079
Siyabonga Nguza <siyabonganguza18@gmail.com>
2024-09-26 16:24:02 +0200

    Login

---
+++ bank/src/main/resources/templates/AuthController.java
@@ -0,0 +1,18 @@
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+
+// @Controller
+// public class AuthController {
+
+//     @GetMapping("/signup")
+//     public String showSignupPage() {
+//         return "signup";  // This maps to signup.html
+//     }
+  
+    
+//     @GetMapping("/")
+//     public String showHomePage() {
+//         // Redirect to login as the default page
+//         return "/signup";
+//     }
+// }
