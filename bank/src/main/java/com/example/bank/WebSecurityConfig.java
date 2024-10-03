aa5a71311ce5a5aee5369817a2e697257581789a
Siyabonga Nguza <siyabonganguza18@gmail.com>
2024-10-03 17:11:36 +0200

    dashbooard html

--- bank/src/main/java/com/example/bank/WebSecurityConfig.java
+++ bank/src/main/java/com/example/bank/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/", "/home").permitAll()
+				.requestMatchers("/", "/Sign").permitAll()
 				.anyRequest().authenticated()
 			)
 			.formLogin((form) -> form
